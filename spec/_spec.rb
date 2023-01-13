8f45fded43bd49407ceec0c3a058421c634b84b8
Weston Sandfort <sandfortw@gmail.com>
2023-01-13 09:39:54 -0700

    Add remaining tests for iteration 3

--- spec/_spec.rb
+++ spec/_spec.rb
@@ -78,13 +78,24 @@ describe Budget do
   it 'can list departments with expenses less than $500' do
     budget = Budget.new("State of Colorado", 1876)
     expect(budget.departments).to eq([])
-    customer_service = Department.new("Customer Service")    
+    customer_service = Department.new("Customer Service")
+    customer_service.expense(500)
     accounting = Department.new("Accounting")    
     budget.add_department(customer_service)
     budget.add_department(accounting)
+    expect(budget.cheap_depts).to eq([accounting])
   end
 
   it 'can list all employees salaries' do 
-
+    budget = Budget.new("State of Colorado", 1876)
+    customer_service = Department.new("Customer Service")
+    accounting = Department.new("Accounting") 
+    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
+    steve = Employee.new({name: "Steve Guy", age: "35", salary: "$30000"})
+    customer_service.hire(bobbi)
+    accounting.hire(steve)
+    budget.add_department(customer_service)
+    budget.add_department(accounting)
+    expect(budget.salaries).to eq({bobbi => bobbi.salary, steve => steve.salary})
   end
 end
