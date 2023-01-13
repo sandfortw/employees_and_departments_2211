1024056aa4be97a46c5f4384a2b565d65a8b25ce
Weston Sandfort <sandfortw@gmail.com>
2023-01-13 09:18:27 -0700

    Add tests for department

--- spec/_spec.rb
+++ spec/_spec.rb
@@ -28,26 +28,33 @@ end
 describe Department do 
 
   it 'has a name' do
-
+    customer_service = Department.new("Customer Service")    
+    expect(customer_service.name).to eq("Customer Service")
   end
 
   it 'has employees blank' do
-
+    customer_service = Department.new("Customer Service")    
+    expect(customer_service.employees).to eq([])
   end
 
-  it 'it can hire' do
-    
+  it 'it can hire and have more employees' do
+    customer_service = Department.new("Customer Service")    
+    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
+    aaron = Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"})  
+    customer_service.hire(bobbi)
+    customer_service.hire(aaron) 
+    expect(customer_service.employees).to eq([bobbi, aaron])
   end
 
-  it 'it has employees full' do
+  
 
+  it 'has expenses' do
+    customer_service = Department.new("Customer Service")    
+    expect(customer_service.expenses).to eq(0)# => 0
+    customer_service.expense(100)
+    customer_service.expense(25)
+    expect(customer_service.expenses).to eq(125)# => 125
   end
 
-  it 'has expenses empty' do
-
-  end
-
-  it 'has expenses full' do
-    
-  end
+  
 end
