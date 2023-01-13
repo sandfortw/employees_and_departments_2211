11788569b22bfa5dfb705d79247630a1aa60c209
Weston Sandfort <sandfortw@gmail.com>
2023-01-13 09:02:46 -0700

    Initial commit

---
+++ spec/_spec.rb
@@ -0,0 +1,21 @@
+require './lib/employee'
+
+describe Employee do
+  
+  it ' has a name' do
+
+  end
+
+  it 'has an age' do
+
+  end
+
+  it 'has a salary' do
+
+  end
+
+  it 'can give a raise' do
+
+  end
+
+end
