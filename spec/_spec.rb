48868ccffe1a62243c422a1314f20cff154f8f43
Weston Sandfort <sandfortw@gmail.com>
2023-01-13 10:12:15 -0700

    Add test for totaling expenses for given employee

--- spec/_spec.rb
+++ spec/_spec.rb
@@ -125,7 +125,26 @@ describe Expense do
     customer_service.add_expense(expense1)
     customer_service.add_expense(expense2)
     expect(customer_service.expense_list).to eq[expense1, expense2]
+    expect(customer_service.expenses).to eq(600)
   end
 
+  it 'can total expenses for a given employee' do
+    budget = Budget.new("State of Colorado", 1876)
+    customer_service = Department.new("Customer Service")
+    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
+    steve = Employee.new({name: "Steve Guy", age: "35", salary: "$30000"})
+    customer_service.hire(bobbi)
+    customer_service.hire(steve)
+
+    expense1 = Expense.new("New Keyboard", bobbi, 100)
+    expense2 = Expense.new("Repair Work", steve, 500)
+    expense3 = Expense.new("New Mouse", bobbi, 110)
+
+    customer_service.add_expense(expense1)
+    customer_service.add_expense(expense2)
+    customer_service.add_expense(expense3)
+    expect(customer_service.total(bobbi)).to eq(210)
+    expect(customer_service.total(steve)).to eq(500)
+  end
 
 end
